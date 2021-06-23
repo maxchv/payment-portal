2c7796078fe218a763de6e5207a448c680bd18d5
Max Shaptala <maxshaptala@gmail.com>
2021-06-23 14:52:54 +0300

    Added tests failed payment

--- src/test/java/com/abank/CreatePaymentTest.java
+++ src/test/java/com/abank/CreatePaymentTest.java
@@ -23,7 +23,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -52,14 +53,17 @@ public class CreatePaymentTest {
         Client client = new Client();
         client.setFirstName("Имя");
         client.setLastName("Фамилия");
+
         Account account1 = new Account();
         account1.setAccountNum("123456789");
         account1.setAccountType("card/simple");
         account1.setBalance(BigDecimal.valueOf(5000));
+
         Account account2 = new Account();
         account2.setAccountNum("987654321");
         account2.setAccountType("card/simple");
         account2.setBalance(BigDecimal.valueOf(10000));
+
         client.addAccount(account1);
         client.addAccount(account2);
         clientRepository.save(client);
@@ -125,9 +129,10 @@ public class CreatePaymentTest {
         assertEquals(destinationAccount.getBalance(), new BigDecimal("10100.00"));
     }
 
+    @SneakyThrows
     @Test
     @Order(3)
-    void createOneFailPaymentResponseTest() throws Exception {
+    void createOneFailPaymentResponseTest() {
         var endpoint = "/api/v1/payment";
         var request = "{\n" +
                 "  \"source_acc_id\": 1,\n" +
@@ -142,10 +147,37 @@ public class CreatePaymentTest {
                 .andExpect(status().isInternalServerError());
     }
 
+    @SneakyThrows
     @Test
     @Order(4)
     void createOneFailPaymentDatabaseTest() {
-        fail("Not implemented yet"); // FIXME: реализовать тест
+        var endpoint = "/api/v1/payment";
+        var request = "{\n" +
+                "  \"source_acc_id\": 1,\n" +
+                "  \"dest_acc_id\": 2,\n" +
+                "  \"amount\": 100000.00,\n" +
+                "  \"reason\": \"назначение платежа\"\n" +
+                "}";
+        mockMvc
+                .perform(post(endpoint)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(status().isInternalServerError());
+        Optional<Payment> paymentOptional = paymentRepository.findById(1L);
+        assertTrue(paymentOptional.isPresent());
+        assertEquals(paymentOptional.get().getStatus(), PaymentStatus.error);
+        assertEquals(paymentOptional.get().getAmount(), new BigDecimal("100000.00"));
+        assertEquals(paymentOptional.get().getReason(), "назначение платежа");
+        assertEquals(paymentOptional.get().getSourceAccount().getId(), 1L);
+        assertEquals(paymentOptional.get().getDestinationAccount().getId(), 2L);
+
+        Optional<Account> sourceAccountOptional = accountRepository.findById(1L);
+        assertTrue(sourceAccountOptional.isPresent());
+        assertEquals(sourceAccountOptional.get().getBalance(), BigDecimal.valueOf(5000));
+
+        Optional<Account> destinationAccountOptional = accountRepository.findById(2L);
+        assertTrue(destinationAccountOptional.isPresent());
+        assertEquals(destinationAccountOptional.get().getBalance(), BigDecimal.valueOf(10000));
     }
 
     @SneakyThrows
@@ -237,16 +269,86 @@ public class CreatePaymentTest {
         assertEquals(destinationAccount.getBalance(), new BigDecimal("9100.00"));
     }
 
+    @SneakyThrows
     @Test
     @Order(6)
     void createTwoFailPaymentResponseTest() {
-        fail("Not implemented yet"); // FIXME: реализовать тест
+        var endpoint = "/api/v1/payments";
+        var request = "[\n" +
+                "  {\n" +
+                "    \"source_acc_id\": 1,\n" +
+                "    \"dest_acc_id\": 2,\n" +
+                "    \"amount\": 100.00,\n" +
+                "    \"reason\": \"назначение платежа\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"source_acc_id\": 2,\n" +
+                "    \"dest_acc_id\": 1,\n" +
+                "    \"amount\": 100000.00,\n" +
+                "    \"reason\": \"назначение платежа\"\n" +
+                "  }\n" +
+                "]";
+        mockMvc
+                .perform(post(endpoint)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].payment_id").value(1))
+                .andExpect(jsonPath("$[0].status").value("ok"))
+                .andExpect(jsonPath("$[1].payment_id").value(2))
+                .andExpect(jsonPath("$[1].status").value("error"));
     }
 
+    @SneakyThrows
     @Test
     @Order(7)
     void createTwoFailPaymentDatabaseTest() {
-        fail("Not implemented yet"); // FIXME: реализовать тест
+        var endpoint = "/api/v1/payments";
+        var request = "[\n" +
+                "  {\n" +
+                "    \"source_acc_id\": 1,\n" +
+                "    \"dest_acc_id\": 2,\n" +
+                "    \"amount\": 100.00,\n" +
+                "    \"reason\": \"назначение платежа\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"source_acc_id\": 2,\n" +
+                "    \"dest_acc_id\": 1,\n" +
+                "    \"amount\": 100000.00,\n" +
+                "    \"reason\": \"назначение платежа\"\n" +
+                "  }\n" +
+                "]";
+        mockMvc
+                .perform(post(endpoint)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(status().isOk());
+
+        Optional<Payment> successPaymentOptional = paymentRepository.findById(1L);
+        assertTrue(successPaymentOptional.isPresent());
+        assertEquals(successPaymentOptional.get().getSourceAccount().getId(), 1L);
+        assertEquals(successPaymentOptional.get().getDestinationAccount().getId(), 2L);
+        assertEquals(successPaymentOptional.get().getAmount(), new BigDecimal("100.00"));
+        assertEquals(successPaymentOptional.get().getReason(), "назначение платежа");
+        assertEquals(successPaymentOptional.get().getStatus(), PaymentStatus.ok);
+
+        Optional<Payment> failPaymentOptional = paymentRepository.findById(2L);
+        assertTrue(failPaymentOptional.isPresent());
+        assertEquals(failPaymentOptional.get().getSourceAccount().getId(), 2L);
+        assertEquals(failPaymentOptional.get().getDestinationAccount().getId(), 1L);
+        assertEquals(failPaymentOptional.get().getAmount(), new BigDecimal("100000.00"));
+        assertEquals(failPaymentOptional.get().getReason(), "назначение платежа");
+        assertEquals(failPaymentOptional.get().getStatus(), PaymentStatus.error);
+
+
+        Optional<Account> firstAccountOptional = accountRepository.findById(1L);
+        assertTrue(firstAccountOptional.isPresent());
+        assertEquals(firstAccountOptional.get().getBalance(), new BigDecimal("4900.00"));
+
+        Optional<Account> secondAccountOptional = accountRepository.findById(2L);
+        assertTrue(secondAccountOptional.isPresent());
+        assertEquals(secondAccountOptional.get().getBalance(), new BigDecimal("10100.00"));
     }
 
 }
